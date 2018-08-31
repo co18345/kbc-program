ab09a68d2d57c6b6bb6d30b59310ee9f679dd4de
saurabh gudwal co18345 <42859089+co18345@users.noreply.github.com>
2018-08-31 09:21:52 -0700

    Add files via upload

---
+++ kbc.c
@@ -0,0 +1,457 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,x,y,z;
+	char p;
+	printf("Welcome to KBC\n");
+	printf("\nMain Amitabh Bachan swaagat karta hu apaka is adbhut khel main.\n");
+	printf("\nAgar aap jithte hain toh aap aaj ghar le jayege EK CRORE RUPEE\n");
+	printf("Varna aap khali hath ghar jayege\n");
+	printf("\nDO YOU WANT TO START THE GAME (y/n)= ");
+	scanf("%c",&p);
+	
+	if (p==121)
+	{	
+	printf("\nToh shubaaramb karte hain is game ka.\n");
+	printf("\nQ1 = Which city is called silicon valley ?    'For 1000Rs'\n");
+	printf("1= Bangalore          ");
+	printf("2= Mumbai\n");
+	printf("3= Delhi              ");
+	printf("4= Calcutta\n");
+	printf("Answer= ");
+	scanf("%d",&a);
+	
+	switch(a)
+	{
+		case (1):
+		printf("\nRight Answer\n");
+		printf("You Won 1000Rs\n");
+		break;
+		
+		case (2):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (3):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (4):
+		printf("\nWrong Answer\n");
+		goto z;
+			
+		default: 
+		printf("\nInvalid Answer\n");
+		goto z;
+	}
+	
+	printf("\nQ2 = The slogan of 'one nation','one tax','one market', was used for promotion of which tax scheme of central govt?    'For 5000Rs'\n");
+	printf("1= GST                2= DDT\n3= STT                4= CST\nAnswer= ");
+	scanf("%d",&b);
+	
+	switch(b)
+	{
+		case (1):
+		printf("\nRight Answer\n");
+		printf("You Won 5000Rs\n");
+		break;
+		
+		case (2):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (3):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (4):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		default: 
+		printf("\nInvalid Answer\n");
+		goto z;
+	}
+	
+	printf("\nQ3 = What is the main ingredient of famous hyderabadi dish 'haleem?'    'For 10000Rs'\n");
+	printf("1= Kekda              2= Fish\n3= Lobster            4= Mutton\nAnswer= ");
+	scanf("%d",&c);
+	
+	switch(c)
+	{
+		case (4):
+		printf("\nRight Answer\n");
+		printf("You Won 10000Rs\n");
+		break;
+		
+		case (1):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (2):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (3):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		default: 
+		printf("\nInvalid Answer\n");
+		goto z;
+	}
+	
+	printf("\nQ4 = What are the ancient pyramids of Giza?    'For 20000Rs'\n");
+	printf("1= Granaries          2= Lighthouse\n3= Tombs              4= Palaces\nAnswer= ");
+	scanf("%d",&d);
+	
+	switch(d)
+	{
+		case (3):
+		printf("\nRight Answer\n");
+		printf("You Won 20000Rs\n");
+		break;
+		
+		case (1):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (2):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (4):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		default: 
+		printf("\nInvalid Answer\n");
+		goto z;
+	}
+	
+	printf("\nQ5 = Which of these is an image file format?    'For 40000Rs'\n");
+	printf("1= MP3                2= AAC\n3= WAV                4= JPEG\nAnswer= ");
+	scanf("%d",&e);
+	
+	switch(e)
+	{
+		case (4):
+		printf("Right Answer\n");
+		printf("You Won 40000Rs\n");
+		break;
+		
+		case (1):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (2):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (3):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		default: 
+		printf("\nInvalid Answer\n");
+		goto z;
+	}
+	
+	printf("\nQ6 = Which of these sports events is not an anual event?    'For 100000Rs'\n");
+	printf("1= Davis Cup                    2= Paralymic Games\n3= Indian Premier League         4= Wimbledon\nAnswer= ");
+	scanf("%d",&f);
+	
+	switch(f)
+	{
+		case (2):
+		printf("Right Answer\n");
+		printf("\nYou Won 100000Rs\n");
+		break;
+		
+		case (1):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (3):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (4):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		default: 
+		printf("\nInvalid Answer\n");
+		goto z;
+	}
+	
+	printf("\nQ7 = Which of the following identity documents uses biometric information?    'For 300000Rs'\n");
+	printf("1= Voter Id           2= Ration Card\n3= Aadhar Card        4= PAN Card\nAnswer= ");
+	scanf("%d",&g);
+	
+	switch(g)
+	{
+		case (3):
+		printf("\nRight Answer\n");
+		printf("You Won 300000Rs\n");
+		break;
+		
+		case (1):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (2):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (4):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		default: 
+		printf("\nInvalid Answer\n");
+		goto z;
+	}
+	
+	printf("\nQ8 = Wich of these disease is caused by protozoan parasite?    'For 1000000Rs'\n");
+	printf("1= Malaria            2= Cholera\n3= Measles            4= Tetanus\nAnswer= ");
+	scanf("%d",&h);
+	
+	switch(h)
+	{
+		case (1):
+		printf("\nRight Answer\n");
+		printf("You Won 1000000Rs\n");
+		break;
+		
+		case (2):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (3):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (4):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		default: 
+		printf("\nInvalid Answer\n");
+		goto z;
+	}
+
+    printf("\nQ9 = Which one of these is not a 21st century invention?    'For 4000000Rs'\n");
+	printf("1= iPhone             2= Google\n3= Skype              4= YouTube\nAnswer= ");
+	scanf("%d",&i);
+	
+	switch(i)
+	{
+		case (2):
+		printf("\nRight Answer\n");
+		printf("You Won 4000000Rs\n");
+		break;
+		
+		case (1):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (3):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (4):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		default: 
+		printf("\nInvalid Answer\n");
+		goto z;
+	}
+	
+	printf("\nQ10 = Which of these social networking sites has a bird as its logo?    'For 5000000Rs'\n");
+	printf("1= Snapchat           2= Tumblr\n3= Twitter            4= WhatsApp\nAnswer= ");
+	scanf("%d",&j);
+	
+	switch(j)
+	{
+		case (3):
+		printf("\nRight Answer\n");
+		printf("You Won 5000000Rs\n");
+		break;
+		
+		case (1):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (2):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (4):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		default: 
+		printf("\nInvalid Answer\n");
+		goto z;
+	}
+	
+	printf("\nQ11 = In football which of these players wears a different colour jersey, from the rest of the team,on the field?    'For 6000000Rs'\n");
+	printf("1= Forward            2= Goalkeeper\n3= Defender           4= Midfielder\nAnswer= \n");
+	scanf("%d",&k);
+	
+	switch(k)
+	{
+		case (2):
+		printf("\nRight Answer\n");
+		printf("You Won 6000000Rs\n");
+		break;
+		
+		case (1):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (3):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (4):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		default: 
+		printf("\nInvalid Answer\n");
+		goto z;
+	}
+	
+	printf("\nQ12 = Which one of these yogasanas is performed by lying on one’s back?    'For 7000000Rs'\n");
+	printf("1= Bhujangasana       2= Padmasana\n3= Garudasana         4= Shavasana\nAnswer= \n");
+	scanf("%d",&l);
+	
+	switch(l)
+	{
+		case (4):
+		printf("\nRight Answer\n");
+		printf("You Won 7000000Rs\n");
+		break;
+		
+		case (1):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (2):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (3):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		default: 
+		printf("\nInvalid Answer\n");
+		goto z;
+	}
+	
+	printf("\nQ13 = Tamarind juice is a key ingredient in which of these dishes?    'For 8000000Rs'\n");
+	printf("1= Idli               2= Dosa\n3= Sambhar            4= Pongal\nAnswer= \n");
+	scanf("%d",&m);
+	
+	switch(m)
+	{
+		case (3):
+		printf("\nRight Answer\n");
+		printf("You Won 8000000Rs\n");
+		break;
+		
+		case (1):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (2):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (4):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		default: 
+		printf("\nInvalid Answer\n");
+		goto z;
+	}
+	
+	printf("\nQ14 = Which is the only animal to be featured in the name of an IPL team?    'For 9000000Rs'\n");
+	printf("1= Horse              2= Tiger\n3= Elephant           4= Lion\nAnswer= \n");
+	scanf("%d",&n);
+	
+    switch(n)
+	{
+		case (4):
+		printf("\nRight Answer\n");
+		printf("You Won 9000000Rs\n");
+		break;
+		
+		case (1):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (2):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (3):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		default: 
+		printf("\nInvalid Answer\n");
+		goto z;
+	}
+	
+	printf("\nQ15 = Fastest animal is?          'For 10000000Rs'\n");
+	printf("1= Cheetah             2= Tiger\n3= Lion                4= Panther\nAnswer= \n");
+    scanf("%d",&o);
+	
+	switch(o)
+	{
+		case (1):
+		printf("\nRight Answer\n");
+		goto y;
+		
+		case (2):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (3):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		case (4):
+		printf("\nWrong Answer\n");
+		goto z;
+		
+		default: 
+		printf("\nInvalid Answer\n");
+		goto z;
+	}
+	
+	z:
+		printf("\nAfsoos galat jawaab\n");
+		printf("Aapka aur hamara safar khatam hota hain yaha par\n",z);
+		goto x;
+		
+	y:
+	    printf("\nCongrats you have won one crore rupees\n");
+		printf("Aap is khel ke vijeta hain\n",y);
+		
+	x:
+	    printf("\nGoodbye\n");
+		printf("See you again");
+	}
+	
+	else
+	{
+		printf("\nGoodbye and see you again");
+	}
+}
